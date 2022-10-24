4b19a630df0c1d6b4f1a8c560a3985cca7fff573
Dexter <dexterious56@gmail.com>
2022-10-23 17:13:09 -0700

    updated routes

--- stock-server/application.py
+++ stock-server/application.py
@@ -52,23 +52,40 @@ def index():
         return data
 
 
+@application.route("/symbol", methods=['GET'])
+def symbol():
+    if flask.request.method == 'GET':
+        res = flask.request.args.get('ticker')
+
+       #res = json.load(res)
+        res = str(res)
+        # print(res)
+        ticker = res if len(res) > 0 else "googl"
+        print("POST:", ticker)
+        prediction = stock_knn.Stock_Price(ticker, SPLIT_RATIO)
+
+        # data = {}
+        data = prediction.high_low()
+
+        # data["5"] = prediction.high_ema(5)
+        # data["10"] = prediction.high_ema(10)
+
+       # return json.dumps(data,indent=2)
+        return data
+
+
 @application.route("/")
 def test():
     return jsonify({"Testing": "test"})
 
 
-@application.route("/title", methods=["POST"])
+@application.route("/title", methods=["GET"])
 def title():
     if flask.request.method == 'POST':
         res = flask.request.get_json()
-
-       #res = json.load(res)
-        res = str(res["ticker"])
-        # print(res)
-        ticker = res if len(res) > 0 else "googl"
-        print("title-Post:", ticker)
+        ticker = str(res["ticker"])
         stock = yf.Ticker(ticker)
-        return stock.info["longName"]
+        return jsonify(stock.info["longName"])
 
 
 if __name__ == "__main__":
